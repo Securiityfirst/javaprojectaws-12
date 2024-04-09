c21523de119c8d2bd04d2dbc35bae2fa1ae0a465
Batume <163580105+Batume@users.noreply.github.com>
2024-04-09 12:09:56 -0500

    My vulnerable application

--- src/main/java/Butler.java
+++ src/main/java/Butler.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 public class Butler {
 
     public void welcome() {
-        ArrayList<String> list1 = new ArrayList<>();
+        ArrayList<String> list1 = new ArrayList<string>();
         list1.add("Hello");
-        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<string>();
         list2.add("World");
         System.out.println(ListUtils.union(list1, list2));
     }
