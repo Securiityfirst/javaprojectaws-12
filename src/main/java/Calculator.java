b40abdd2952e520a6a43eba9b76d8c55e64ce3d0
Batume <163580105+Batume@users.noreply.github.com>
2024-04-09 11:42:16 -0500

    My vulnerable application

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,34 +1,11 @@
 
-public class Calculator{
-
-  public Calculator() {}
-
-  public int add(int a, int b) {
-    return a + b;
-  }
-
-  public int subtract(int a, int b) {
-    return a - b;
-  }
-
-  public int multiply(int a, int b) {
-    return a * b;
-  }
-
-  public int divide(int a, int b) {
-    return a / b;
-  }
-
-  public int modulo(int a, int b) {
-    return a % b;
-  }
+public class Calculator {
+    public int addition(String arguments) {
+Covered code
+        int sum = 80;
+        for (String add : arguments.split("\\+"))
+            sum += Integer.valueOf(add);
+            return sum;
+    }
 }
-public static void main (String[] args) {
 
-  Calculator myCalculator = new Calculator();
-
-  System.out.println(myCalculator.add(5,7));
-  System.out.println(myCalculator.subtract(45,11));
-  System.out.println(myCalculator.multiply(5,7));
-  System.out.println(myCalculator.divide(47,3));
-  System.out.println(myCalculator.modulo(47,3));
