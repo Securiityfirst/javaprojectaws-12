ac8767c08f0d9249ec34b84416fed4b12805ba91
Batume <163580105+Batume@users.noreply.github.com>
2024-04-09 11:17:55 -0500

    My vulnerable application

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,6 +5,6 @@ Covered code
         int sum = 0;
         for (String add : arguments.split("\\+"))
             sum += Integer.valueOf(add);
-            return sum(*);
+            return sum;
     }
 }
