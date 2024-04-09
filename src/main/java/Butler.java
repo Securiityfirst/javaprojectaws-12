0be69b334e78ef83d8ee24a1d2f4688f1150476f
Batume <163580105+Batume@users.noreply.github.com>
2024-04-09 11:13:43 -0500

    My vulnerable application

--- src/main/java/Butler.java
+++ src/main/java/Butler.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 public class Butler {
 
     public void welcome() {
-        ArrayList<String> list1 = new ArrayList<String>();
+        ArrayList<String> list1 = new ArrayList<>();
         list1.add("Hello");
-        ArrayList<String> list2 = new ArrayList<String>();
+        ArrayList<String> list2 = new ArrayList<>();
         list2.add("World");
         System.out.println(ListUtils.union(list1, list2));
     }
--- src/main/java/Unzipper.java
+++ src/main/java/Unzipper.java
@@ -1,15 +1,29 @@
 import org.nd4j.util.ArchiveUtils;
 import java.io.File;
-
 public class Unzipper {
     public static void main(String[] args) throws Exception {
-
         Butler butler = new Butler();
         butler.welcome();
+        // https://app.snyk.io/vuln/SNYK-JAVA-ORGND4J-72550
+        // should appear as Reachable
         ArchiveUtils.unzipFileTo("./malicious_file.zip", "./unzipped/");
-        File f = new File("/tmp/evil.txt");
+        File f = new File("/myDirectory/myfile.txt");  // Compliant
+
+File.createTempFile("prefix", "suffix", new File("/mySecureDirectory"));  // Compliant
+
+if(SystemUtils.IS_OS_UNIX) {
+  FileAttribute<Set<PosixFilePermission>> attr = PosixFilePermissions.asFileAttribute(PosixFilePermissions.fromString("rwx------"));
+  Files.createTempFile("prefix", "suffix", attr); // Compliant
+}
+else {
+  File f = Files.createTempFile("prefix", "suffix").toFile();  // Compliant
+  f.setReadable(true, true);
+  f.setWritable(true, true);
+  f.setExecutable(true, true);
+}
         if (f.exists()) {
             throw new Exception("Malicious file /tmp/evil.txt was created");
         };
     }
 }
+
